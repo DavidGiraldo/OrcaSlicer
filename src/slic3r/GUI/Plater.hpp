66e837d69993d850829cc7a06ee5fc11f05f71de
Maxellis93 <max.ellis@9spokes.com>
2026-07-16 11:35:54 +1200

    feat(remote-api): POST /slice + GET /slice/status with live progress/stats (M3 Task 10); declare EVT_PROCESS_COMPLETED for controller binding
    (cherry picked from commit 084bdc7e15feca7adbf0355faf705eae7c5a8905)

--- src/slic3r/GUI/Plater.hpp
+++ src/slic3r/GUI/Plater.hpp
@@ -51,6 +51,7 @@ class SLAPrint;
 class PartPlateList;
 class SlicingStatusEvent;
 class BackgroundSlicingProcess;
+class SlicingProcessCompletedEvent;
 enum SLAPrintObjectStep : unsigned int;
 enum class ConversionType : int;
 class DevAms;
@@ -91,6 +92,7 @@ enum class ActionButtonType : int;
 
 //BBS: add EVT_SLICING_UPDATE declare here
 wxDECLARE_EVENT(EVT_SLICING_UPDATE, Slic3r::SlicingStatusEvent);
+wxDECLARE_EVENT(EVT_PROCESS_COMPLETED, Slic3r::SlicingProcessCompletedEvent);
 wxDECLARE_EVENT(EVT_PUBLISH,        wxCommandEvent);
 wxDECLARE_EVENT(EVT_OPEN_PLATESETTINGSDIALOG,        wxCommandEvent);
 

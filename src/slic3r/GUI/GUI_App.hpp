39e9fa3bbca2bf546aa6c28928480514c4e61ef6
Maxellis93 <max.ellis@9spokes.com>
2026-07-16 02:48:55 +1200

    feat(remote-api): embedded HTTP server skeleton with token auth

--- src/slic3r/GUI/GUI_App.hpp
+++ src/slic3r/GUI/GUI_App.hpp
@@ -17,6 +17,7 @@
 #include "slic3r/GUI/HMS.hpp"
 #include "slic3r/GUI/Jobs/UpgradeNetworkJob.hpp"
 #include "slic3r/GUI/HttpServer.hpp"
+#include "RemoteAPI/RemoteAPIServer.hpp"
 #include "../Utils/PrintHost.hpp"
 
 #include <wx/app.h>
@@ -318,6 +319,7 @@ private:
     bool             m_show_error_msgdlg{false};
     wxString         m_info_dialog_content;
     HttpServer       m_http_server;
+    RemoteAPI::Server m_remote_api_server;
     bool             m_show_gcode_window{true};
     boost::thread    m_check_network_thread;
 public:
@@ -513,6 +515,9 @@ public:
     void            start_http_server();
     void            start_http_server(int port);
     void            stop_http_server();
+    void            start_remote_api();
+    void            stop_remote_api();
+    RemoteAPI::Server &remote_api_server() { return m_remote_api_server; }
     void            switch_staff_pick(bool on);
 
     void            on_show_check_privacy_dlg(int online_login = 0);
